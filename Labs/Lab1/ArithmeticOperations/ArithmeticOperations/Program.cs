9cb6705f4fd8644e2f6c2e0c80d103b4addb3905
Alex Vasil <alexandervasil840@gmail.com>
2020-09-03 00:19:27 -0400

    Lab1

---
+++ Labs/Lab1/ArithmeticOperations/ArithmeticOperations/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArithmeticOperations {
+    class Program {
+        static void Main(string[] args)
+        {
+            int number1 = 20; 
+            int number2 = 4; 
+            int sum; 
+            int difference; 
+            int product; 
+            int quotient; 
+            int remainder; 
+
+            sum = number1 + number2; 
+            difference = number1 - number2; 
+            product = number1 * number2; 
+            quotient = number1 / number2; 
+            remainder = number1 % number2; 
+
+            Console.WriteLine("The sum of {0} and {1} is {2}", number1, number2, sum); 
+            Console.WriteLine("The difference between {0} and {1} is {2}", number1, number2, difference); 
+            Console.WriteLine("The product of {0} and {1} is {2}", number1, number2, product); 
+            Console.WriteLine("{0} divided by {1} is {2}", number1, number2, quotient); 
+            Console.WriteLine("The remainder of {0} divided by {1} is {2}", number1, number2, remainder);
+        }
+    }
+}
---
+++ Labs/Lab1/CompareNames/CompareNames/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareNames {
+    class Program {
+        static void Main(string[] args)
+        {
+            String name1 = "Andrew";
+            String name2 = "Andrew";
+            String name3 = "Martin";
+
+            Console.WriteLine("compare {0} to {1} : {2}", name1, name2, name1 == name2); 
+            Console.WriteLine("compare {0} to {1} : {2}", name1, name3, name1 == name3); 
+            Console.WriteLine();
+
+            Console.WriteLine("{0} and {1} : Equals() method gives {2}", name1, name2, name1.Equals(name2)); 
+            Console.WriteLine("{0} and {1} : Equals() method gives {2}", name1, name3, name1.Equals(name3)); 
+            Console.WriteLine(); 
+            Console.WriteLine("{0} and {1} : CompareTo() method gives {2}", name1, name2, name1.CompareTo(name2)); 
+            Console.WriteLine("{0} and {1} : CompareTo() method gives {2}", name1, name3, name1.CompareTo(name3)); Console.WriteLine(); 
+            Console.WriteLine("{0} and {1} : Compare() method gives {2}", name1, name2, String.Compare(name1, name2)); 
+            Console.WriteLine("{0} and {1} : Compare() method gives {2}", name1, name3, String.Compare(name1, name3));
+        }
+    }
+}
